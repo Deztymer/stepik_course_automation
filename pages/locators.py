04226a8002d9c80519c39ceda0beb5507d168819
Deztymer <54896680+Deztymer@users.noreply.github.com>
2022-11-26 15:33:22 +0300

    Add files via upload

---
+++ pages/locators.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    GO_TO_BASKET_BTN = (By.CSS_SELECTOR, ".btn-group>a[href*='basket']")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
+class BasketPageLocators():
+    EMPTY_BASKET_TEXT = (By.CSS_SELECTOR, "#content_inner p")
+    BASKET_ITEMS_HEADER = (By.CSS_SELECTOR, ".basket-items")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASS = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PROVE_PASS = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BTN = (By.NAME, "registration_submit")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, ".add-to-basket button")
+    ADDED_PRODUCT_TEXT = (By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
+    BASKET_PRICE_TEXT = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_NAME_TEXT = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE_TEXT = (By.CSS_SELECTOR, ".product_main .price_color")
+
+
+class CardPageLocators(object):
+    BASKET_ITEMS = (By.CSS_SELECTOR, '.basket-items')
+    MESSAGE_IN_BASKET = (By.CSS_SELECTOR, '#content_inner')
